e5c400489c74916feb54195e17fe2ad7ede31b44
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-08-06 14:35:51 -0400

    message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
+// import { Container, Navbar, NavbarBrand } from 'reactstrap';
+// // import NucampLogo from './app/assets/img/logo.png';
 // import CampsiteCard from './features/counter/campsites/CampsiteCard';
+import Header from './components/Header';
 import CampsitesList from './features/counter/campsites/CampsitesList';
 import { CAMPSITES } from './app/shared/CAMPSITES';
 import './App.css';
@@ -9,13 +10,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Navbar dark color='primary' sticky='top' expand='md'>
+      {/* { <Navbar dark color='primary' sticky='top' expand='md'>
         <Container>
           <NavbarBrand href='/'>
             <img src={NucampLogo} alt='nucamp logo' />
           </NavbarBrand>
         </Container>
-      </Navbar>
+      </Navbar> } */}
+      <Header/>
       <CampsitesList campsite={CAMPSITES[0]} />
     </div>
   );
