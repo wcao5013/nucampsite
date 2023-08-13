52aa4360d7b98a15d1d95605a0c2915698be1540
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-08-12 20:39:48 -0400

    message

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,30 +1,13 @@
 import { Container } from 'reactstrap';
-// import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CampsitesList from '../features/campsites/CampsitesList';
-// import { selectCampsiteById } from '../features/campsites/campsitesSlice';
-// import { useState } from 'react';
 
-const CampsitesDirectoryPage = () => {
-    // const [campsiteId, setCampsiteId] = useState(0);
-    // const selectedCampsite = selectCampsiteById(campsiteId);
-    
+
+const CampsitesDirectoryPage = () => {   
     return (
-        // <Container>
-            
-        //     <Row>
-        //         <Col sm='5' md='7'>
-        //             <CampsitesList setCampsiteId={setCampsiteId} />
-        //         </Col>
-        //         <Col sm='7' md='5'>
-        //             <CampsiteDetail campsite={selectedCampsite}/>
-        //         </Col>
-        //     </Row>
-        // </Container>
-
         <Container>
             <CampsitesList />
         </Container>
-    )
-}
+    );
+};
 
 export default CampsitesDirectoryPage;
