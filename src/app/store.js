02cc664dfaab282d1567b3408e8fd701736f97bc
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-08-30 20:26:37 -0400

    message

--- src/app/store.js
+++ src/app/store.js
@@ -4,12 +4,14 @@ import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnerReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import { userReducer } from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
     campsites: campsitesReducer,
     comments: commentsReducer,
     partners: partnerReducer,
-    promotions: promotionsReducer
+    promotions: promotionsReducer,
+    user: userReducer
   },
 });
